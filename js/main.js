9cfeea9720b9540d32a644e78801c97aa03e8665
sneeeg <61973865+sneeeg@users.noreply.github.com>
2020-10-05 14:29:45 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(".burger_menu, a").click(function(e){
+    e.preventDefault();
+    alert('Еще ниче не готово че ты тыкаешь')
+});
+$(".card a").click(function(e){
+    e.preventDefault();
+    alert('Еще ниче не готово че ты тыкаешь')
+});
